5853c4144fd6e124e8f983d7dfe83b22749c0d12
Has4276* <attaulhasnainmaqsoodi@gmail.com>
2025-08-22 10:56:58 +0500

    Add active state styling and transition effects to navbar links; enhance button interactions in the CTA section

--- JS/app.js
+++ JS/app.js
@@ -4,3 +4,16 @@ const today = new Date();
 let year = today.getFullYear()
 
 document.getElementById("year").textContent = year
+
+// Navbar active toggle
+const anchors = document.querySelectorAll(".nav");
+
+const active = (e) => {
+    anchors.forEach(a => {
+        a.classList.add("active");
+    })
+    this.classList.remove("active")
+}
+anchors.forEach(anchor => {
+    anchor.addEventListener("click", active)
+});
