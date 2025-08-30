a1e3dae46fdc85d3b95dcae395e88dedc4b7a128
Muhammad_Atta_Ul_Hasnain <attaulhasnainmaqsoodi@gmail.com>
2025-08-30 12:27:01 +0500

    Add error handling for empty input and update result display structure

--- JS/fetchData.js
+++ JS/fetchData.js
@@ -1,16 +1,18 @@
-const API_KEY = "3f4bbc7fd68ddceeb2cf6791";
+const API_KEY = "d7ab7502d13d1ed63e868897";
 
 const StdEndPoint = `https://v6.exchangerate-api.com/v6/${API_KEY}`;
 const PairEndPoint = `https://v6.exchangerate-api.com/v6/${API_KEY}/pair/EUR/GBP`;
 const enrichedData = `https://v6.exchangerate-api.com/v6/${API_KEY}/enriched/GBP/PKR`
 const supportedCodes = `https://v6.exchangerate-api.com/v6/${API_KEY}/codes`
 
-const conversion_btn = document.getElementById("conversion_btn");
-const amountInput = document.getElementById("amountInput");
+const conversion_btn = document.querySelector("#conversion_btn");
+const amountInput = document.querySelector("#amountInput");
 const base_select = document.querySelector("#base-select");
 const target_select = document.querySelector("#target-select");
 const conversionResult = document.querySelector("#conversion-result")
 const conversionRateSec = document.querySelector("#conversion-rate-sec")
+const errorMsgSec = document.querySelector(".errorMsg")
+const resultSec = document.querySelector("#result")
 
 let base_label_text = "From";
 let target_label_text = "To";
@@ -59,41 +61,54 @@ document.addEventListener("DOMContentLoaded", async () => {
         conversion_btn.addEventListener("click", async e => {
             e.preventDefault();
 
-            // const PairAmountEndPoint = `${StdEndPoint}/pair/PKR/USD/${amountInput.value}`;
-            const PairAmountEndPoint = `${StdEndPoint}/pair/${base_select.value}/${target_select.value}/${amountInput.value}`;
-            try {
-                let response = await fetch(PairAmountEndPoint)
-                // console.log(response);
-                let result = await response.json();
-                // console.log(result);
-                // console.log(base_select.children[0].value);
-                // console.log(target_select.children[0].value);
+            if (amountInput.value == 0) {
 
-                conversionResult_label.textContent = `${amountInput.value} ${base_select.value} =`
-                let conversion_result = result?.conversion_result
-                conversionResult.textContent = `${conversion_result} ${target_select.value}`;
+                // conversion_btn.setAttribute("disabled")
+                resultSec.classList.add("hidden")
+                errorMsgSec.classList.remove("hidden")
+                errorMsgSec.innerHTML = `<p class="amount_errorMsg ">Please enter some amount!</p>`
+            } else if (amountInput.value > 0) {
+                // if (amountInput.value > 0) {
 
+                errorMsgSec.classList.add("hidden")
+                resultSec.classList.remove("hidden")
+
+                // const PairAmountEndPoint = `${StdEndPoint}/pair/PKR/USD/${amountInput.value}`;
+                const PairAmountEndPoint = `${StdEndPoint}/pair/${base_select.value}/${target_select.value}/${amountInput.value}`;
                 try {
-                    const baseToTrgt = await fetch(`${StdEndPoint}/pair/${base_select.value}/${target_select.value}/1`)
-                    const updtData = await baseToTrgt.json()
-                    console.log(updtData);
-                    conversionRateSec.innerHTML = `<p>1 ${updtData?.base_code} = ${updtData?.conversion_rate} ${updtData?.target_code}</p>`
+                    let response = await fetch(PairAmountEndPoint)
+                    // console.log(response);
+                    let result = await response.json();
+                    // console.log(result);
+                    // console.log(base_select.children[0].value);
+                    // console.log(target_select.children[0].value);
+
+                    conversionResult_label.textContent = `${amountInput.value} ${base_select.value} =`
+                    let conversion_result = result?.conversion_result
+                    conversionResult.textContent = `${conversion_result} ${target_select.value}`;
+
+                    try {
+                        const baseToTrgt = await fetch(`${StdEndPoint}/pair/${base_select.value}/${target_select.value}/1`)
+                        const updtData = await baseToTrgt.json()
+                        console.log(updtData);
+                        conversionRateSec.innerHTML = `<p>1 ${updtData?.base_code} = ${updtData?.conversion_rate} ${updtData?.target_code}</p>`
+                    } catch (error) {
+                        console.error(error);
+                    }
+
+                    try {
+                        const trgtToBase = await fetch(`${StdEndPoint}/pair/${target_select.value}/${base_select.value}/1`);
+                        const updtData = await trgtToBase.json();
+                        console.log(updtData);
+                        conversionRateSec.innerHTML += `<p>1 ${updtData?.base_code} = ${updtData?.conversion_rate} ${updtData?.target_code}</p>`
+
+                    } catch (error) {
+                        console.error(error);
+                    }
+
                 } catch (error) {
-                    console.error(error);
+                    console.error(error)
                 }
-
-                try {
-                    const trgtToBase = await fetch(`${StdEndPoint}/pair/${target_select.value}/${base_select.value}/1`);
-                    const updtData = await trgtToBase.json();
-                    console.log(updtData);
-                    conversionRateSec.innerHTML += `<p>1 ${updtData?.base_code} = ${updtData?.conversion_rate} ${updtData?.target_code}</p>`
-
-                } catch (error) {
-                    console.error(error);
-                }
-
-            } catch (error) {
-                console.error(error)
             }
         })
 
@@ -103,6 +118,9 @@ document.addEventListener("DOMContentLoaded", async () => {
         swap_btn.addEventListener("click", async e => {
             e.preventDefault()
 
+            if (amountInput.value == 0) {
+                conversion_btn.setAttribute("disabled")
+            }
             // Dropdown Values swapping
             let temp = base_select.value;
             base_select.value = target_select.value
